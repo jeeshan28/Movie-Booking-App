cebe2c039e57fb71015a7a814724d85bf36c0e02
jeeshan28 <kjeeshan312@gmail.com>
2024-03-07 14:36:03 +0530

    Movie Booking App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react'
+import Header from './components/Header';
+import { Route, Routes } from 'react-router-dom';
+import HomePage from './components/HomePage';
+import Movies from './components/Movies/Movies';
+import Admin from './components/Auth/Admin';
+import Auth from './components/Auth/Auth'
+import { useDispatch, useSelector } from 'react-redux';
+import { adminActions, userActions } from './store';
+import Booking from './components/Bookings/Booking';
+import UserProfile from './profile/UserProfile';
+import AddMovie from './components/Movies/AddMovie';
+import AdminProfile from './profile/AdminProfile';
 
-function App() {
+const App = () => {
+  const dispatch = useDispatch();
+  const isAdminLoggedIn = useSelector((state) => state.admin.isLoggedIn);
+  const isUserLoggedIn = useSelector((state) => state.user.isLoggedIn);
+  console.log("isAdminLoggedIn", isAdminLoggedIn);
+  console.log("isUserLoggedIn", isUserLoggedIn);
+  useEffect(() => {
+    if (localStorage.getItem("userId")) {
+      dispatch(userActions.login());
+    } else if (localStorage.getItem("adminId")) {
+      dispatch(adminActions.login());
+    }
+  }, [dispatch])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <section>
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/movies' element={<Movies />} />
+          {!isUserLoggedIn && !isAdminLoggedIn && (
+            <>
+              {" "}
+              <Route path='/admin' element={<Admin />} />
+              <Route path='/auth' element={<Auth />} />
+            </>
+          )}
+
+          {isUserLoggedIn && !isAdminLoggedIn && (
+            <>
+              {" "}
+              <Route path='/user' element={<UserProfile />} />
+              <Route path='/booking/:id' element={<Booking />} />
+            </>
+          )}
+          {isAdminLoggedIn && !isUserLoggedIn && (
+            <>
+              {" "}
+              <Route path='/add' element={<AddMovie />} />
+              <Route path='/user-admin' element={<AdminProfile />} />
+            </>
+          )}
+        </Routes>
+      </section>
     </div>
-  );
+  )
 }
 
 export default App;
